92fa3de6fccb5f0e956ee17c020ccc0901bb6543
kuamillll <blastergmly@gmail.com>
2023-11-22 11:29:30 +0700

    assets dibuat

--- resources/views/Destinations/index.blade.php
+++ resources/views/Destinations/index.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="Traveloka.png">
+    <title>Traveloka</title>
+</head>
+<body>
+    <ul class="nav">
+        <li class="nav-item">
+            <a href="{{url('/')}}" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="{{url('show')}}" class="nav-link">Home</a>
+        </li>
+    </ul>
+</body>
+</html>
