13a84c2fecb56482ffe6213f7fce0b985aaf1321
kuamillll <blastergmly@gmail.com>
2023-11-22 11:32:21 +0700

    ubah dikit

--- resources/views/Destinations/index.blade.php
+++ resources/views/Destinations/index.blade.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" type="image/png" href="Traveloka.png">
+    <title>Traveloka</title>
 </head>
 <body>
-    
+    <ul class="nav">
+        <li class="nav-item">
+            <a href="{{url('/')}}" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="{{url('show')}}" class="nav-link">Home</a>
+        </li>
+    </ul>
 </body>
 </html>
